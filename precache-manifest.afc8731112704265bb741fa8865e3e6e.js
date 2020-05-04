1b2729e0a87c6452f95d29048ff47b8bc1ec1c08
Olubisi David <oludavidconnect@gmail.com>
2020-05-04 16:44:17 +0100

    Updates

---
+++ precache-manifest.afc8731112704265bb741fa8865e3e6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83933c1905ba34a71a58dae94647f78d",
+    "url": "/wisdomgate/index.html"
+  },
+  {
+    "revision": "93e295542e85b7f99c89",
+    "url": "/wisdomgate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "94ffab947c6351e063bf",
+    "url": "/wisdomgate/static/js/2.efcbf874.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wisdomgate/static/js/2.efcbf874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e295542e85b7f99c89",
+    "url": "/wisdomgate/static/js/main.3e17e3df.chunk.js"
+  },
+  {
+    "revision": "92528043771d095316d7",
+    "url": "/wisdomgate/static/js/runtime-main.fc78e44a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wisdomgate/static/media/logo.5d5d9eef.svg"
+  }
+]);
